0bc19bbd4f0eecd1f81a10f7d35b9a18bc6d9f1f
Matthew Caravagio <matthew.caravaggio@protonmail.com>
2020-07-05 19:48:47 -0400

    Completed update, looking to QoL changes and css

--- client/src/components/Guest.jsx
+++ client/src/components/Guest.jsx
@@ -26,7 +26,31 @@ export default class Guest extends Component {
      onDeleteGuestClick = () => {
         const guestId = this.props.guestId
         axios.delete(`/api/guestBook/${guestId}`)
-     }    
+            .then(() => {
+                this.refreshGuest()
+            })
+     }
+     
+    //  update
+    updateGuest = () => {
+        const guestId = this.props.guestId
+        axios.put(`/api/guestBook/${guestId}`, {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            message: this.state.message
+        })
+            .then(() => {
+                this.refreshGuest()
+            })
+    }
+
+    onGuestChange = (event) => {
+        const updateGuest = {...this.state}
+        updateGuest[event.target.name] = event.target.value
+        this.setState({firstName: updateGuest.firstName,
+                        lastName: updateGuest.lastName,
+                        message: updateGuest.message})
+    }
 
     render() {
         const {firstName, lastName, message} = this.state
@@ -34,6 +58,36 @@ export default class Guest extends Component {
                 <div>
                     <h2 className='guest-names'>{firstName}<br/>{lastName}</h2>
                     <p className='message'>{message}</p>
+                    <form id='update-guest-form'>
+                        <input
+                            className='input-field'
+                            type='string'
+                            name='firstName'
+                            placeholder='First Name'
+                            required='required'
+                            onChange={this.onGuestChange}
+                            value={this.state.firstName}/>
+                        <input
+                            className='input-field'
+                            type='string'
+                            name='lastName'
+                            placeholder='Last Name'
+                            required='required'
+                            onChange={this.onGuestChange}
+                            value={this.state.lastName}/>
+                        <input
+                            className='input-field'
+                            type='string'
+                            name='message'
+                            placeholder='Message'
+                            required='required'
+                            onChange={this.onGuestChange}
+                            value={this.state.message}/>
+                    </form>
+                    <button
+                        onClick={() => this.updateGuest()}>
+                        Update
+                    </button>
                     <button onClick={this.onDeleteGuestClick}>
                         Delete
                     </button>
